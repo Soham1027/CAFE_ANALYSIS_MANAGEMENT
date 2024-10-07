df1667e1a8d905f4a1f34c8036e8a54549e71e51
soham1027 <sohamghayal02@gmail.com>
2024-10-07 15:22:06 +0530

    asdxfghj

--- cafe_analysis_project/cafe_analysis_app/process_video.py
+++ cafe_analysis_project/cafe_analysis_app/process_video.py
@@ -20,7 +20,7 @@ class VideoProcessor:
         if self.video_thread and self.video_thread.is_alive():
             return False  # Already running
         self.stop_event.clear()
-        self.cap = cv2.VideoCapture(self.stream_url)  # Initialize capture object here
+        self.cap = cv2.VideoCapture(0)  # Initialize capture object here
         self.video_thread = threading.Thread(target=self.process_video)
         self.video_thread.start()
         return True
@@ -72,7 +72,7 @@ class VideoProcessor:
         logger.info("Live video processing finished and resources cleaned up")
 
 # Initialize global video processor for the stream
-video_processor = VideoProcessor('cafe_analysis_app/1.mp4')
+video_processor = VideoProcessor(0)
 
 @csrf_exempt
 def start_video_processing(request):
@@ -99,7 +99,7 @@ def generate_frames(stream_url):
     # Load models (ensure the models are quantized if possible)
     yolo_net, classes, age_net, gender_net = load_models()
 
-    cap = cv2.VideoCapture(stream_url)
+    cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         logger.error(f"Failed to open video stream {stream_url}")
         return
@@ -137,5 +137,5 @@ def generate_frames(stream_url):
 
 
 def video_feed(request):
-    stream_url = 'cafe_analysis_app/1.mp4'
+    stream_url = 0
     return StreamingHttpResponse(generate_frames(stream_url), content_type='multipart/x-mixed-replace; boundary=frame')
