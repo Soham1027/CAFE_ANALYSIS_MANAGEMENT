a0a57b5dc48e5a6b46f062694c9eb4288ad37ea2
Soham1027 <sohamghayal02@gmail.com>
2024-10-11 00:23:34 +0530

    changed

--- cafe_analysis_project/cafe_analysis_project/settings.py
+++ cafe_analysis_project/cafe_analysis_project/settings.py
@@ -77,15 +77,22 @@ WSGI_APPLICATION = 'cafe_analysis_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#         'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'cafe_db',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#         }
+# }
+
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cafe_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'localhost',
-        'PORT':'3306',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
 # Password validation
